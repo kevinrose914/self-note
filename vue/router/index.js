3096ef517604ef9642c8c6b32203dd43cb320ca4
kevinrose914 <1507037715@qq.com>
2019-04-12 20:37:32 +0800

    xxx

--- vue/router/index.js
+++ vue/router/index.js
@@ -72,13 +72,6 @@ function getLocation (base = '') {
     }
     return (path || '/') + window.location.search + window.location.hash
 }
-function changeUrl(base, path, replace) {
-    if (replace) {
-      window.history.replaceState({}, '', (base + path).replace(/\/\//g, '/'))
-    } else {
-      window.history.pushState({}, '', (base + path).replace(/\/\//g, '/'))
-    }
-}
 
 class Base {
     constructor(router) {
@@ -93,8 +86,10 @@ class Base {
         }
     }
     transitionTo(target, cb) {
+        // 找到匹配的路由配置
         const targetRoute = match(target, this.router.routes)
         this.confirmTransition(targetRoute, () => {
+            // 这儿在每次赋值时，就会出发watcher进行更新
             this.current.route = targetRoute
             this.current.name = targetRoute.name
             this.current.path = targetRoute.path
@@ -103,9 +98,10 @@ class Base {
             cb && cb()
         })
     }
+    // 执行路由钩子函数，然后执行跳转后的回调函数
     confirmTransition(route, cb) {
         // 暂时不加钩子函数
-        cb()
+        cb && cb()
     }
 }
 
@@ -113,6 +109,7 @@ class HTML5Hisotry extends Base {
     constructor(router) {
         super(router)
         window.addEventListener('popstate', () => {
+            console.log('pop state..')
             this.transitionTo(getLocation())
         })
     }
@@ -120,64 +117,80 @@ class HTML5Hisotry extends Base {
         const targetRoute = match(location, this.router.routes)
 
         this.transitionTo(targetRoute, () => {
-            debugger;
-            changeUrl(this.router.base, this.current.fullPath)
+            this.changeUrl(this.router.base, this.current.fullPath)
         })
     }
     replace(location) {
         const targetRoute = match(location, this.router.routes)
 
         this.transitionTo(targetRoute, () => {
-            changeUrl(this.router.base, this.current.fullPath, true)
+            this.changeUrl(this.router.base, this.current.fullPath, true)
         })
     }
     go(n) {
         window.history.go(n)
     }
-    getCurrentLocation () {
+    getCurrentPath () {
         return getLocation(this.router.base)
+    }
+    changeUrl(base, path, replace) {
+        if (replace) {
+          window.history.replaceState({}, '', (base + path).replace(/\/\//g, '/'))
+        } else {
+          window.history.pushState({}, '', (base + path).replace(/\/\//g, '/'))
+        }
     }
 }
 
 class HashHistroy extends Base {
     constructor (router) {
-      super(router)
-      this.ensureSlash()
-      window.addEventListener('hashchange', () => {
-        this.transitionTo(this.getCurrentLocation())
-      })
+        super(router)
+        this.ensureSlash()
+        window.addEventListener('hashchange', () => {
+            this.transitionTo(this.getCurrentLocation())
+        })
     }
   
     push (location) {
-      const targetRoute = match(location, this.router.routes)
-  
-      this.transitionTo(targetRoute, () => {
-        changeUrl(this.current.fullPath.substring(1))
-      })
+        const targetRoute = match(location, this.router.routes)
+    
+        this.transitionTo(targetRoute, () => {
+            this.changeUrl(this.current.fullPath.substring(1))
+        })
     }
   
     replaceState (location) {
-      const targetRoute = match(location, this.router.routes)
-  
-      this.transitionTo(targetRoute, () => {
-        changeUrl(this.current.fullPath.substring(1), true)
-      })
+        const targetRoute = match(location, this.router.routes)
+    
+        this.transitionTo(targetRoute, () => {
+            this.changeUrl(this.current.fullPath.substring(1), true)
+        })
     }
   
-  
+    // 确保以#开头
     ensureSlash () {
-      const path = this.getCurrentLocation()
-      if (path.charAt(0) === '/') {
-        return true
-      }
-      changeUrl(path)
-      return false
+        const path = this.getCurrentPath()
+        if (path.charAt(0) === '/') {
+            return true
+        }
+        this.changeUrl(path)
+        return false
     }
   
-    getCurrentLocation() {
-      const href = window.location.href
-      const index = href.indexOf('#')
-      return index === -1 ? '' : href.slice(index + 1)
+    getCurrentPath() {
+        const href = window.location.href
+        const index = href.indexOf('#')
+        return index === -1 ? '' : href.slice(index + 1)
+    }
+    changeUrl(path, replace) {
+        const href = window.location.href
+        const index = href.indexOf('#')
+        const base = index > -1 ? href.slice(0, index) : href
+        if (replace) {
+            window.history.replaceState({}, '', `${base}#/${path}`)
+        } else {
+            window.history.pushState({}, '', `${base}#/${path}`)
+        }
     }
 }
 
@@ -220,7 +233,7 @@ class Router {
         new window.Observer(this.history.current)
         // 监听this.history.current.route的变化，改变视图，要用hash模式才能看得出来变化，history模式需要服务端支持
         new window.Watcher(this.history.current, 'route', this.render.bind(this))
-        history.transitionTo(history.getCurrentLocation())
+        history.transitionTo(history.getCurrentPath())
     }
 }
 
