61a7e8e534e817bebed275376792d94df1c9211b
kevinrose914 <1507037715@qq.com>
2019-12-04 10:12:52 +0800

    lodash函数去抖节流源码分析

--- js/函数柯里化/科利华.js
+++ js/函数柯里化/科利华.js
@@ -1,6 +1,7 @@
 (function(root) {
-    var _ = {}
-    root._ = _
+    var _ = {};
+    root._ = _;
+    // fn允许多少个参数，而返回的函数多传的参数就合并为一个参数
     root._.restArg = function(fn) {
         return function () {
             var alowLength = fn.length
